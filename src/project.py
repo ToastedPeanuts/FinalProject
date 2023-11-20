f22aee381612712abcab00050dd988f30d822c05
Shano <samueljohnburke@yahoo.com>
2023-11-20 15:21:26 -0600

    setting up def main

--- src/project.py
+++ src/project.py
@@ -1,4 +1,20 @@
+import pygame
+
 def main():
+    pygame.init()
+    pygame.display.set_caption('Character')
+    resolution=(800,600)
+    screen=pygame.display.set_mode(resolution)
+    running=True
+    while running:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                running=False
+        #Game Logic:
+        #Render and Display
+        screen.fill('Green')
+        pygame.flip()
+    pygame.quit()
 
 
 if __name__ == "__main__":
