ee67d06acd43dfb2101bdbcce586a44c22a17e9b
Shano <samueljohnburke@yahoo.com>
2023-11-20 19:39:56 -0600

    attempting to put text on top of images

--- src/project.py
+++ src/project.py
@@ -5,7 +5,6 @@ import time
 def draw_text(screen,text,font,text_col,x,y):
     img = font.render(text, True, text_col)
     screen.blit(img,(x,y))
-    text_font=pygame.font.SysFont("Arial", 30)
 
 def anim_print(text):
     for character in text:
@@ -20,10 +19,11 @@ def main():
     imp = pygame.image.load("DarkWoods.png")
     screen.blit(imp,(0,0))
     pygame.display.flip()
+    text_font=pygame.font.SysFont("Arial", 30)
 
-    
     running=True
     while running:
+        draw_text(imp,"Hello there brave adventurer!", text_font, (255,255,255), 400,420)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 running=False
