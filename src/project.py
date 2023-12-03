6325fabe946361ce04bb98e5ed0db8b85a5711e0
Shano <samueljohnburke@yahoo.com>
2023-12-03 17:24:05 -0600

    Music volume

--- src/project.py
+++ src/project.py
@@ -608,6 +608,7 @@ if __name__ == "__main__":
     pygame.mixer.init()
     pygame.mixer.music.load('ForestSounds.mp3')
     pygame.mixer.music.play()
+    pygame.mixer.music.set_volume(0.2)
     anim_print("What is your name?\n")
     # Input from user 
     global name
